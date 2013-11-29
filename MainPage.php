2f7af2d47b5aac40ace023aaca914de29e0e75ff
Gabbeh <gabichan@me.com>
2013-11-29 16:49:03 -0700

    removed existing js scripts from header

--- MainPage.php
+++ MainPage.php
@@ -1,12 +1,11 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" />
+       // <meta charset="utf-8" />
         <title>Book a Book Catalogue</title>
-        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-        <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-        <link rel="stylesheet" href="/resources/demos/style.css" />
+        //<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+       // <script type="text/javascript" src="jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.js"></script>
+       // <link href="jquery-ui-1.10.3.custom/css/ui-lightness/jquery-ui-1.10.3.custom.css" rel="stylesheet" type="text/css" />
         <style>
 
             .custom-combobox {
