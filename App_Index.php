8b5453849cbd85943680944fae4a438cc909f7a9
sjdixon <sjdixon2@gmail.com>
2013-11-29 16:20:06 -0700

    Fixed formatting for some files. Added librarian.html Added some links.

--- App_Index.php
+++ App_Index.php
@@ -1,47 +1,46 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<!--Main page of our web application. Contains the tab framework and script files needed for the rest of the app-->
-	<!--Gaby Comeau, November 22, 2013-->
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script type="text/javascript" src="jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.js"></script>
-	<link href="jquery-ui-1.10.3.custom/css/ui-lightness/jquery-ui-1.10.3.custom.css" rel="stylesheet" type="text/css" />
-	<!--Other CSS elements to style headers and links not included in the JQuery CSS page-->
-	<style>
-	h1 {font-family: Verdana,Arial,sans-serif; color: #1C94C4}
-	a {font-family: Verdana,Arial,sans-serif;}
-	div > div > table {border-collapse:collapse;}
-	td {padding:3px 30px 3px 3px;}
-    </style>
-    
-	<script>
-	$(function() {
-		$( "#tabs" ).tabs();
-	});
-	</script>
-</head> 
-<body>
-<h1>Library Home</h1>
-<div id = "tabs">
-	<ul>
-		<li> <a href="#tabs-1">Catalog Search</a></li>
-		<li> <a href="PatronTab.php">Patrons</a></li>
-		<!--this tab is linked to a php script- this script therefore prints its' content in div tabs-3>-->
-		<li> <a href="inventory.php">Inventory</a></li>
-		<li> <a href="#tabs-4">Add Loans/Holds</a></li>
-                <li> <a href="#tabs-5">Librarian</a></li>
-	</ul>
-	<div id="tabs-1">
-	</div>
-	<div id="tabs-2">
-	</div>
-	<div id="tabs-3">
-	</div>
-	<div id ="tabs-4">
-	</div>
-        <div id ="tabs-5">
-	</div>
-</div>
-</body>
+    <head>
+        <!--Main page of our web application. Contains the tab framework and script files needed for the rest of the app-->
+        <!--Gaby Comeau, November 22, 2013-->
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script type="text/javascript" src="jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.js"></script>
+        <link href="jquery-ui-1.10.3.custom/css/ui-lightness/jquery-ui-1.10.3.custom.css" rel="stylesheet" type="text/css" />
+        <!--Other CSS elements to style headers and links not included in the JQuery CSS page-->
+        <style>
+            h1 {font-family: Verdana,Arial,sans-serif; color: #1C94C4}
+            a {font-family: Verdana,Arial,sans-serif;}
+            div > div > table {border-collapse:collapse;}
+            td {padding:3px 30px 3px 3px;}
+        </style>
+
+        <script>
+            $(function() {
+                $("#tabs").tabs();
+            });
+        </script>
+    </head> 
+    <body>
+        <h1>Library Home</h1>
+        <div id = "tabs">
+            <ul>
+                <li> <a href="MainPage.php">Catalog Search</a></li>
+                <li> <a href="PatronTab.php">Patrons</a></li>
+                <!--this tab is linked to a php script- this script therefore prints its' content in div tabs-3>-->
+                <li> <a href="inventory.php">Inventory</a></li>
+                <li> <a href="#tabs-4">Add Loans/Holds</a></li>
+                <li> <a href="librarian.html">Librarian</a></li>
+            </ul>
+            <div id="tabs-1">
+            </div>
+            <div id="tabs-2">
+            </div>
+            <div id="tabs-3">
+            </div>
+            <div id ="tabs-4">
+            </div>
+            <div id ="tabs-5">
+            </div>
+        </div>
+    </body>
 </html>
-	
--- MainPage.php
+++ MainPage.php
@@ -1,214 +1,214 @@
 <!doctype html>
 <html lang="en">
-<head>
-<meta charset="utf-8" />
-<title>Book a Book Catalogue</title>
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-<link rel="stylesheet" href="/resources/demos/style.css" />
-<style>
-    
-.custom-combobox {
-position: relative;
-display: inline-block;
-}
-.custom-combobox-toggle {
-position: absolute;
-top: 0;
-bottom: 0;
-margin-left: -1px;
-padding: 0;
-/* support: IE7 */
-*height: 1.7em;
-*top: 0.1em;
-}
-.custom-combobox-input {
-margin: 0;
-padding: 0.3em;
-}
-#parent {
-width: 60%;
-height: 40%;
-margin: 10px auto;
-padding: 5px;
-border: 1px solid #777;
-background-color: #fbca93;
-text-align: center;
-}
-.positionable {
-position: absolute;
-display: block;
-right: 0;
-bottom: 0;
-background-color: #bcd5e6;
-text-align: center;
-}
-select, input {
-margin-left: 15px;
-}
-</style>
-<script>
-(function( $ ) {
-$.widget( "custom.combobox","custom.combobox2", {
-_create: function() {
-this.wrapper = $( "<span>" )
-.addClass( "custom-combobox" )
-.insertAfter( this.element );
-this.element.hide();
-this._createAutocomplete();
-this._createShowAllButton();
-},
-_createAutocomplete: function() {
-var selected = this.element.children( ":selected" ),
-value = selected.val() ? selected.text() : "";
-this.input = $( "<input>" )
-.appendTo( this.wrapper )
-.val( value )
-.attr( "title", "" )
-.addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left" )
-.autocomplete({
-delay: 0,
-minLength: 0,
-source: $.proxy( this, "_source" )
-})
-.tooltip({
-tooltipClass: "ui-state-highlight"
-});
-this._on( this.input, {
-autocompleteselect: function( event, ui ) {
-ui.item.option.selected = true;
-this._trigger( "select", event, {
-item: ui.item.option
-});
-},
-autocompletechange: "_removeIfInvalid"
-});
-},
-_createShowAllButton: function() {
-var input = this.input,
-wasOpen = false;
-$( "<a>" )
-.attr( "tabIndex", -1 )
-.attr( "title", "Show All Items" )
-.tooltip()
-.appendTo( this.wrapper )
-.button({
-icons: {
-primary: "ui-icon-triangle-1-s"
-},
-text: false
-})
-.removeClass( "ui-corner-all" )
-.addClass( "custom-combobox-toggle ui-corner-right" )
-.mousedown(function() {
-wasOpen = input.autocomplete( "widget" ).is( ":visible" );
-})
-.click(function() {
-input.focus();
-// Close if already visible
-if ( wasOpen ) {
-return;
-}
-// Pass empty string as value to search for, displaying all results
-input.autocomplete( "search", "" );
-});
-},
-_source: function( request, response ) {
-var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
-response( this.element.children( "option" ).map(function() {
-var text = $( this ).text();
-if ( this.value && ( !request.term || matcher.test(text) ) )
-return {
-label: text,
-value: text,
-option: this
-};
-}) );
-},
-_removeIfInvalid: function( event, ui ) {
-// Selected an item, nothing to do
-if ( ui.item ) {
-return;
-}
-// Search for a match (case-insensitive)
-var value = this.input.val(),
-valueLowerCase = value.toLowerCase(),
-valid = false;
-this.element.children( "option" ).each(function() {
-if ( $( this ).text().toLowerCase() === valueLowerCase ) {
-this.selected = valid = true;
-return false;
-}
-});
-// Found a match, nothing to do
-if ( valid ) {
-return;
-}
-// Remove invalid value
-this.input
-.val( "" )
-.attr( "title", value + " didn't match any item" )
-.tooltip( "open" );
-this.element.val( "" );
-this._delay(function() {
-this.input.tooltip( "close" ).attr( "title", "" );
-}, 2500 );
-this.input.data( "ui-autocomplete" ).term = "";
-},
-_destroy: function() {
-this.wrapper.remove();
-this.element.show();
-}
-});
-})( jQuery );
-$(function() {
-$( "#combobox" ).combobox();
-$( "#combobox2" ).combobox();
-});    
-    
-$(function() {
-function position() {
+    <head>
+        <meta charset="utf-8" />
+        <title>Book a Book Catalogue</title>
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <link rel="stylesheet" href="/resources/demos/style.css" />
+        <style>
 
-$( "#parent" ).position({
-});
-}
-})
+            .custom-combobox {
+                position: relative;
+                display: inline-block;
+            }
+            .custom-combobox-toggle {
+                position: absolute;
+                top: 0;
+                bottom: 0;
+                margin-left: -1px;
+                padding: 0;
+                /* support: IE7 */
+                *height: 1.7em;
+                *top: 0.1em;
+            }
+            .custom-combobox-input {
+                margin: 0;
+                padding: 0.3em;
+            }
+            #parent {
+                width: 60%;
+                height: 40%;
+                margin: 10px auto;
+                padding: 5px;
+                border: 1px solid #777;
+                background-color: #fbca93;
+                text-align: center;
+            }
+            .positionable {
+                position: absolute;
+                display: block;
+                right: 0;
+                bottom: 0;
+                background-color: #bcd5e6;
+                text-align: center;
+            }
+            select, input {
+                margin-left: 15px;
+            }
+        </style>
+        <script>
+            (function($) {
+                $.widget("custom.combobox", "custom.combobox2", {
+                    _create: function() {
+                        this.wrapper = $("<span>")
+                                .addClass("custom-combobox")
+                                .insertAfter(this.element);
+                        this.element.hide();
+                        this._createAutocomplete();
+                        this._createShowAllButton();
+                    },
+                    _createAutocomplete: function() {
+                        var selected = this.element.children(":selected"),
+                                value = selected.val() ? selected.text() : "";
+                        this.input = $("<input>")
+                                .appendTo(this.wrapper)
+                                .val(value)
+                                .attr("title", "")
+                                .addClass("custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left")
+                                .autocomplete({
+                                    delay: 0,
+                                    minLength: 0,
+                                    source: $.proxy(this, "_source")
+                                })
+                                .tooltip({
+                                    tooltipClass: "ui-state-highlight"
+                                });
+                        this._on(this.input, {
+                            autocompleteselect: function(event, ui) {
+                                ui.item.option.selected = true;
+                                this._trigger("select", event, {
+                                    item: ui.item.option
+                                });
+                            },
+                            autocompletechange: "_removeIfInvalid"
+                        });
+                    },
+                    _createShowAllButton: function() {
+                        var input = this.input,
+                                wasOpen = false;
+                        $("<a>")
+                                .attr("tabIndex", -1)
+                                .attr("title", "Show All Items")
+                                .tooltip()
+                                .appendTo(this.wrapper)
+                                .button({
+                                    icons: {
+                                        primary: "ui-icon-triangle-1-s"
+                                    },
+                                    text: false
+                                })
+                                .removeClass("ui-corner-all")
+                                .addClass("custom-combobox-toggle ui-corner-right")
+                                .mousedown(function() {
+                                    wasOpen = input.autocomplete("widget").is(":visible");
+                                })
+                                .click(function() {
+                                    input.focus();
+        // Close if already visible
+                                    if (wasOpen) {
+                                        return;
+                                    }
+        // Pass empty string as value to search for, displaying all results
+                                    input.autocomplete("search", "");
+                                });
+                    },
+                    _source: function(request, response) {
+                        var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+                        response(this.element.children("option").map(function() {
+                            var text = $(this).text();
+                            if (this.value && (!request.term || matcher.test(text)))
+                                return {
+                                    label: text,
+                                    value: text,
+                                    option: this
+                                };
+                        }));
+                    },
+                    _removeIfInvalid: function(event, ui) {
+        // Selected an item, nothing to do
+                        if (ui.item) {
+                            return;
+                        }
+        // Search for a match (case-insensitive)
+                        var value = this.input.val(),
+                                valueLowerCase = value.toLowerCase(),
+                                valid = false;
+                        this.element.children("option").each(function() {
+                            if ($(this).text().toLowerCase() === valueLowerCase) {
+                                this.selected = valid = true;
+                                return false;
+                            }
+                        });
+        // Found a match, nothing to do
+                        if (valid) {
+                            return;
+                        }
+        // Remove invalid value
+                        this.input
+                                .val("")
+                                .attr("title", value + " didn't match any item")
+                                .tooltip("open");
+                        this.element.val("");
+                        this._delay(function() {
+                            this.input.tooltip("close").attr("title", "");
+                        }, 2500);
+                        this.input.data("ui-autocomplete").term = "";
+                    },
+                    _destroy: function() {
+                        this.wrapper.remove();
+                        this.element.show();
+                    }
+                });
+            })(jQuery);
+            $(function() {
+                $("#combobox").combobox();
+                $("#combobox2").combobox();
+            });
 
-$(function() {
-        $( "input[type=submit], a, button" )
-        .button()
-        .click(function( event ) {
-        event.preventDefault();
-        });
-        });
-</script>
-</head>
-<body>
-    <a Href=" " <label> Login </label> </a>  
-<div id="parent">
-<p>
-Catalogue
-<br />
-<select id="combobox">
-<option value="">Type</option>
-<option value="Book">Book</option>
-<option value="Audio">Audio</option>
-<option value="Video">Video</option>
-<option value="Newspaper">Newspaper</option>
-<option value="Magazine">Magazine</option>
-</select>
-<input id="Search" value1=""/>
-<select id="combobox">
-<option value1="">Select one...</option>
-<option value1="Title">Title</option>
-<option value1="year">Year</option>
-<option value1="Asp">Genre</option>
-<option value1="libraryId">Book Code</option>
-</select>
-<input type="submit"  value="Search" />
-</p>
+            $(function() {
+                function position() {
 
-</div>
-</body>
+                    $("#parent").position({
+                    });
+                }
+            })
+
+            $(function() {
+                $("input[type=submit], a, button")
+                        .button()
+                        .click(function(event) {
+                            event.preventDefault();
+                        });
+            });
+        </script>
+    </head>
+    <body>
+        <a Href=" " <label> Login </label> </a>  
+        <div id="parent">
+            <p>
+                Catalogue
+                <br />
+                <select id="combobox">
+                    <option value="">Type</option>
+                    <option value="Book">Book</option>
+                    <option value="Audio">Audio</option>
+                    <option value="Video">Video</option>
+                    <option value="Newspaper">Newspaper</option>
+                    <option value="Magazine">Magazine</option>
+                </select>
+                <input id="Search" value1=""/>
+                <select id="combobox">
+                    <option value1="">Select one...</option>
+                    <option value1="Title">Title</option>
+                    <option value1="year">Year</option>
+                    <option value1="Asp">Genre</option>
+                    <option value1="libraryId">Book Code</option>
+                </select>
+                <input type="submit"  value="Search" />
+            </p>
+
+        </div>
+    </body>
 </html>
