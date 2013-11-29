6aa493377a8b34e2b78adaa90879e22ec8e436b4
Gabbeh <gabichan@me.com>
2013-11-29 16:15:05 -0700

    Added patron management tab to main page.

--- PatronTab.php
+++ PatronTab.php
@@ -1,36 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Book a Book - Patron </title>
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-<link rel="stylesheet" href="/resources/demos/style.css">
-
-<style>
-body { font-size: 62.5%; }
-label, input { display:block; }
-input.text { margin-bottom:12px; width:30%; padding: .4em; }
-fieldset { padding:0; border:0; margin-top:25px; }
-h1 { font-size: 1.2em; margin: .6em 0; }
-div#users-contain { width: 350px; margin: 20px 0; }
-div#users-contain table { margin: 1em 0; border-collapse: collapse; width: 100%; }
-div#users-contain table td, div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
-.ui-dialog .ui-state-error { padding: .3em; }
-.validateTips { border: 1px solid transparent; padding: 0.3em; }
-
- #selected { margin-top: 2em; }
-</style>
-
- <script>
+<script>
 $(function() {
 $( "#selected" ).buttonset();
 });
 </script>
-
-</head>
-<body>
 
 <div id="selected">
 <input type="checkbox" id="check1">
@@ -70,5 +42,3 @@ $( "#selected" ).buttonset();
 </tbody>
 </table>
 </div>
-</body>
-</html>
