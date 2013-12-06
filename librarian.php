787b4606fe86ff3703fa833847f8ce3f1a3b9a97
sjdixon <sjdixon2@gmail.com>
2013-12-06 13:01:10 -0700

    Added php that automatically populates the initial table. Some defects exist.

---
+++ librarian.php
@@ -0,0 +1,163 @@
+<!-- 
+Librarian User Management Page
+By: Stephen Dixon
+-->
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Manage Librarians</title>
+        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+        <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+        <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+        <style>
+            body { font-size: 62.5%; }
+            label, input { display:block; }
+            input.text { margin-bottom:12px; width:95%; padding: .4em; }
+            fieldset { padding:0; border:0; margin-top:25px; }
+            h1 { font-size: 1.2em; margin: .6em 0; }
+            div#users-contain { width: 350px; margin: 20px 0; }
+            div#users-contain table { margin: 1em 0; border-collapse: collapse; width: 100%; }
+            div#users-contain table td, div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
+            .ui-dialog .ui-state-error { padding: .3em; }
+            .validateTips { border: 1px solid transparent; padding: 0.3em; }
+        </style>
+        <script>
+            $(function() {
+                var name = $("#name"),
+                        username = $("#username"),
+                        id=$("#idBox"),
+                        allFields = $([]).add(name).add(username).add(id),
+                        tips = $(".validateTips");
+                function updateTips(t) {
+                    tips
+                            .text(t)
+                            .addClass("ui-state-highlight");
+                    setTimeout(function() {
+                        tips.removeClass("ui-state-highlight", 1500);
+                    }, 500);
+                }
+                function checkLength(o, n, min, max) {
+                    if (o.val().length > max || o.val().length < min) {
+                        o.addClass("ui-state-error");
+                        updateTips("Length of " + n + " must be between " +
+                                min + " and " + max + ".");
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+                function checkRegexp(o, regexp, n) {
+                    if (!(regexp.test(o.val()))) {
+                        o.addClass("ui-state-error");
+                        updateTips(n);
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+                $("#dialog-form").dialog({
+                    autoOpen: false,
+                    height: 300,
+                    width: 350,
+                    modal: true,
+                    buttons: {
+                        "Create an account": function() {
+                            var bValid = true;
+                            allFields.removeClass("ui-state-error");
+                            bValid = bValid && checkLength(name, "name", 3, 20);
+                            bValid = bValid && checkLength(username, "username", 3, 16);
+                            bValid = bValid && checkRegexp(username, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter.");
+                            // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+                            //bValid = bValid && checkRegexp(email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com");
+
+                            $.ajax({
+                                type: "post",
+                                url: "addLibrarian.php",
+                                data: "name=" + name + 
+                                        "&username=" + username +
+                                        "&id=" + id + 
+                                        "&valid=" + bValid;
+                            });
+
+                            if (bValid) {
+                                $("#users tbody").append("<tr>" +
+                                        "<td>" + id.val()+ "</td>" +
+                                        "<td>" + name.val() +"</td>"
+                                        "<td>" + username.val() + "</td>" +
+                                        "</tr>");
+                                $(this).dialog("close");
+                            }
+                        },
+                        Cancel: function() {
+                            $(this).dialog("close");
+                        }
+                    },
+                    close: function() {
+                        allFields.val("").removeClass("ui-state-error");
+                    }
+                });
+                $("#create-user")
+                        .button()
+                        .click(function() {
+                            $("#dialog-form").dialog("open");
+                        });
+            });
+        </script>
+    </head>
+
+    <body>
+        <div id="dialog-form" title="Create new user">
+            <p class="validateTips">All form fields are required.</p>
+            <form>
+                <fieldset>
+                    <label for="idBox">id</label>
+                    <input type="text" name="idBox" id="idBox" class="text ui-widget-content ui-corner-all">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" class="text ui-widget-content ui-corner-all">
+                </fieldset>
+            </form>
+        </div>
+        <div id="users-contain" class="ui-widget">
+            <h1>Existing Users:</h1>
+
+            <?php
+            $server = mysql_connect("localhost","ubuntu", "stephen123");
+            $db = mysql_select_db("library", $server);
+            $query = mysql_query("select * from Librarian");
+
+            ?>
+
+
+            <table id="users" class="ui-widget ui-widget-content">
+                <thead>
+                    <tr class="ui-widget-header ">
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+
+                    while ($row=mysql_fetch_array($query)){
+                    echo "<tr>";
+                    echo "<td>".$row[id]."</td>";
+                    echo "<td>".$row[name]."</td>";
+                    echo "<td>".$row[startDate]."</td>";
+                    echo "<td>".$row[endDate]."</td>";
+                    echo "<td>".$row[username]."</td>";
+                    echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <button id="create-user">Create new user</button>
+    </body>
+</html>
