ef1d260f1fc15e985594447c87800bfe88b71bbd
Gabbeh <gabichan@me.com>
2013-12-09 13:45:59 -0700

    Modified deleteCopy.php to avoid users accidentally deleting all instances if the stock number is blank by changing the item number to -1

--- Processing/deleteCopy.php
+++ Processing/deleteCopy.php
@@ -1,7 +1,7 @@
 <?php	
 	//deleteCopy.php - written by Gaby Comeau
 	//PHP script to delete an item instance from the database
-	header("Location: ../App_Index.php",TRUE,303);	
+	//header("Location: ../App_Index.php",TRUE,303);	
 	$host = "localhost";
 	$user = "ubuntu";
 	$pass = "stephen123";				
@@ -10,12 +10,14 @@
 	
 	$item = $_POST['lCode2'];
 	$copy = $_POST['stockNum2'];
+	if ($copy == '') $item = -1; //this will cause the query to fail, so you don't delete every instance of an item if you forget the stock number
+	echo "Item: $item";
 	
-	$query = "DELETE FROM Item_Instance WHERE (libraryCode = '$item' AND stockNum = '$copy')";
-	echo "Query: $query<br>";
-	$results = mysql_query($query);
-	if(!$results){
-      	echo "could not delete instance <br />";
-    	trigger_error(mysql_error(), E_USER_ERROR);
-    }
+	//$query = "DELETE FROM Item_Instance WHERE (libraryCode = '$item' AND stockNum = '$copy')";
+	//echo "Query: $query<br>";
+	//$results = mysql_query($query);
+	//if(!$results){
+      //	echo "could not delete instance <br />";
+    //	trigger_error(mysql_error(), E_USER_ERROR);
+    //}
 ?>
