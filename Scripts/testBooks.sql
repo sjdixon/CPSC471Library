2d5dcfedefc10963a5811319314f0859d8b69d91
sjdixon <sjdixon2@gmail.com>
2013-12-09 10:02:19 -0700

    Fixed defect where @v1 was not updating properly

--- Scripts/testBooks.sql
+++ Scripts/testBooks.sql
@@ -1,12 +1,14 @@
 use library;
 
 # Book
-set @v1 := (select max(i.libraryCode) from Item i) + 1;
-insert into item values (@v1,'Book','Fantasy Section','Harry Potter and the Philosophers Stone','1997',0,"Fantasy", "Children");
-insert into Book (libraryCode, ISBN, author) values (@v1, 0563533390, "JK Rowling");
-set @v1 = @v1 + 1
-insert into item values (@v1,'Book','Fantasy Section','Harry Potter and the Chamber of Secrets','1997',0,"Fantasy", "Children");
-insert into Book (libraryCode, ISBN, author) values (@v1, 1408810557, "JK Rowling");
-set @v1 = @v1 + 1
-insert into item values (@v1,'Book','Fantasy Section','Harry Potter and the Prisoner of Azkaban','1997',0,"Fantasy", "Children");
-insert into Book (libraryCode, ISBN, author) values (@v1, 1408810565, "JK Rowling");
+delete from Book where libraryCode > 1;
+delete from Item where libraryCode > 1;
+set @v1 = (select max(i.libraryCode) from Item as i) + 1;
+insert into Item values (@v1,'Book','Fantasy Section','Harry Potter and the Philosophers Stone','1997',0,"Fantasy", "Children");
+insert into Book (libraryCode, ISBN, authors) values (@v1, 0563533390, "JK Rowling");
+set @v1 = @v1 + 1;
+insert into Item values (@v1,'Book','Fantasy Section','Harry Potter and the Chamber of Secrets','1998',0,"Fantasy", "Children");
+insert into Book (libraryCode, ISBN, authors) values (@v1, 1408810557, "JK Rowling");
+set @v1 = @v1 + 1;
+insert into Item values (@v1,'Book','Fantasy Section','Harry Potter and the Prisoner of Azkaban','2000',0,"Fantasy", "Children");
+insert into Book (libraryCode, ISBN, authors) values (@v1, 1408810565, "JK Rowling");
