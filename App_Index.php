42f7e355fa5ad9ac04a5517c49fea7fd6326b8dd
sjdixon <sjdixon2@gmail.com>
2013-12-15 20:24:02 -0700

    Added loanHold.php

--- App_Index.php
+++ App_Index.php
@@ -32,7 +32,7 @@
                 <li> <a href="PatronTab.php">Patrons</a></li>
                 <!--this tab is linked to a php script- this script therefore prints its' content in div tabs-3>-->
                 <li> <a href="inventory.php">Inventory</a></li>
-                <li> <a href="#tabs-4">Add Loans/Holds</a></li>
+                <li> <a href="loanHold.php">Add Loans/Holds</a></li>
                 <li> <a href="librarian.php">Librarian</a></li>
             </ul>
             <div id="tabs-1">
