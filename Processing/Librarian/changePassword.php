4384a34128b69179bcd895e8c8f9b553c4dddff3
sjdixon <sjdixon2@gmail.com>
2013-12-19 16:47:58 -0700

    Corrected error in changePassword.php that prevented it from uploading shit.

--- Processing/Librarian/changePassword.php
+++ Processing/Librarian/changePassword.php
@@ -8,7 +8,6 @@ mysql_select_db("library");
 
 // Info
 $username = mysql_real_escape_string($_POST['username']);
-echo $_POST['authPass'];
 $authPass = md5(mysql_real_escape_string($_POST['authPass']));
 $password = md5(mysql_real_escape_string($_POST['password']));
 $repeat = md5(mysql_real_escape_string($_POST['repeat']));
@@ -20,22 +19,25 @@ $legitimate = FALSE;
 $checkLoginQuery = "SELECT username, password from Librarian where username='$username' and password='$authPass'";
 $checkLogin = mysql_query($checkLoginQuery);
 $num = mysql_num_rows($checkLogin);
-if (mysql_num_rows($checkLogin)==1){
+if (mysql_num_rows($checkLogin) == 1) {
     $legitimate = TRUE;
-}
-else {
+} else {
     $checkRootQuery = "SELECT username, password from Librarian where username='ubuntu' and password='$authPass'";
     $checkRoot = mysql_query($checkRootQuery);
-    if (mysql_num_rows($checkRoot)==1){
+    if (mysql_num_rows($checkRoot) == 1) {
         $legitimate = TRUE;
     }
 }
-
+echo "<br/>";
 echo $legitimate ? 'true' : 'false';
+echo "<br/>";
 
-if ($legitimate == TRUE && $password==$repeat) {
+if ($legitimate == TRUE && $password == $repeat) {
     $query = "Update Librarian set password='$password' where username='$username'";
-    $result = mysqli_query($query);
+    $result = mysql_query($query);
+    echo "$query <br/>";
+    echo $result;
+    if (!mysql_error()) print 'all is fine';
     header("Location: ../../librarian.php");
 } else {
     header("Location: ../../Error/401.php");
