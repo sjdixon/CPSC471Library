4fdc834ab384cb7291a780c33d987da38c6f8e97
sjdixon <sjdixon2@gmail.com>
2013-12-18 19:29:50 -0700

    Committing bug fix so that hold and loans are using now() instead of curdate() Also committed better error messaging for process.php

--- Processing/Loans/process.php
+++ Processing/Loans/process.php
@@ -31,13 +31,23 @@ if ($patronIdType != "pAccount") {
 }
 
 // Check if patron is allowed to place holds or loans
+$legal = TRUE;
 $legalPatronQuery = "SELECT membershipExpiryDate, membershipExpired from Patron where pAccount=$pAccount and membershipExpiryDate < curdate()";
 $patronExpired = mysql_query($legalPatronQuery);
-$legal = TRUE;
+$patronHasFines = "SELECT pAccount, balance from Fine where pAccount=$pAccount and balance > 0";
+$fines = mysql_query($patronHasFines);
 $patronExists = mysql_query("SELECT pAccount from Patron where pAccount=$pAccount");
-if (mysql_num_rows($patronExpired) != 0 || mysql_num_rows($patronExists) == 0) {
+if (mysql_num_rows($patronExpired) != 0) {
     $legal = FALSE;
-    echo "Patron with id $pAccount has an expired account or does not exist.";
+    echo "Patron with id $pAccount has an expired account.";
+}
+if (mysql_num_rows($patronExists)==0){
+    $legal = FALSE;
+    echo "Patron with id $pAccount does not exist.";
+}
+if (mysql_num_rows($fines)!=0){
+    $legal = FALSE;
+    echo "Patron has fines.";
 }
 
 
@@ -74,7 +84,7 @@ if ($legal == TRUE) {
         if ($numRows == 0 || ($numRows > 0 && $row[0] == $pAccount)) {
             // Either no hold exists, or the patron who held it is loaning the item.
             $dueDate = $_POST['dueDate']; // if there is one
-            $query = "INSERT INTO Loan values($pAccount,$stock,$libraryCode, curdate(),'$dueDate', NULL)";
+            $query = "INSERT INTO Loan values($pAccount,$stock,$libraryCode, now(),'$dueDate', NULL)";
             // Resolve the hold.
             $updateQuery = "UPDATE Hold set pickupDate=now() where libraryCode=$libraryCode and stocknum=$stock and pAccount=$pAccount and pickupDate is NULL";
             mysql_query($updateQuery);
@@ -99,19 +109,17 @@ if ($legal == TRUE) {
             $dateAvail = "now()";
             $expiryDate = "NOW() + INTERVAL $timeToPickup";
             $query = "INSERT INTO Hold (pAccount, libraryCode, dateHeld, stocknum, availDate, expiryDate) " .
-                    "values($pAccount, $libraryCode, curdate(), $stocknum, $dateAvail, $expiryDate)";
+                    "values($pAccount, $libraryCode, now(), $stocknum, $dateAvail, $expiryDate)";
         } else {
 
             // If not, place the item on hold and set requestDate to curdate.
             $query = "INSERT INTO Hold (pAccount, libraryCode, dateHeld, timeToPickup) " .
-                    "values($pAccount, $libraryCode, curdate(), '$timeToPickup')";
+                    "values($pAccount, $libraryCode, now(), '$timeToPickup')";
         }
     }
 
-
     echo "$query <br/>";
     $result = mysql_query($query);
-
 
     if (!$result) {
         echo $dueDate;
@@ -125,7 +133,6 @@ if ($legal == TRUE) {
     }
 } else {
     echo "Patron $pAccount expired at time $row[0]";
-    
 }
 ?>
 
