fb4125ffc3de374195dbbc5f44bdb919c51db2fc
Gabbeh <gabichan@me.com>
2013-12-20 10:38:52 -0700

    Committing changes to Processing and inventory

--- Processing/getItem.php
+++ Processing/getItem.php
@@ -47,7 +47,7 @@
 	echo "<label for=\"name5\">Title: </label>";
 	echo "<input type =\"text\" name=\"name5\" id=\"name5\" class=\"text ui-widget-content ui-corner-all\" value = \"$row[3]\" /><br><br>";
 	echo "<label for=\"spinner6\">Release Year: </label>";
-	echo "<input id=\"spinner6\" name=\"spinner5\" value=\"$row[4]\" /><br><br>";
+	echo "<input id=\"spinner6\" name=\"spinner6\" value=\"$row[4]\" /><br><br>";
 	echo "<label for=\"genre5\">Genre: </label>";
 	echo "<input type =\"text\" name=\"genre5\" id=\"genre5\" class=\"text ui-widget-content ui-corner-all\" value = \"$row[6]\" /><br><br>";
 	echo "<label for=\"audience5\">Audience: </label>";
@@ -110,4 +110,5 @@
 	echo "<button type=\"submit\" name=\"submit\" value=\"Submit\">OK</button>";	
 echo "</form>";
 ?>
+
 
