8d22f1530b01c42a44e7907ea418d2372b66c117
Rhianne Hadfield <dawg_rh@hotmail.com>
2013-12-19 20:21:28 -0700

    Add include statment

--- Processing/Patron/CancelHold.php
+++ Processing/Patron/CancelHold.php
@@ -1,9 +1,6 @@
 <?php
 $host = "localhost";
-	$user = "ubuntu";
-	$pass = "stephen123";				
-	mysql_connect($host, $user, $pass) or die("Could not connect: " . mysql_error());
-	mysql_select_db("library");
+	include '../../Headers/dbConnect.php';
         foreach($_POST['lCode']as $item){
             
         $pId=$_COOKIE['patronAccount'];
