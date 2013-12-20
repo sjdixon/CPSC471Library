eb2f0bc1c509912eca27f912d45245636547934d
Rhianne Hadfield <dawg_rh@hotmail.com>
2013-12-20 13:31:48 -0700

    Added Session

--- returnItem.php
+++ returnItem.php
@@ -1,3 +1,16 @@
+
+<?php session_start();
+        // If the user is not logged then the user will be set to the main page
+        if (isset($_SESSION['loggedIn']) && isset($_SESSION['username'])) {
+          if($_SESSION["loggedIn"] !=1)
+          {
+              header("Location: MainPage.php");
+          }
+        }
+        else{
+            header("Location: MainPage.php");
+        }
+?>
 //<html>
 //    <body>
 //        <title>Item Returns</title>
