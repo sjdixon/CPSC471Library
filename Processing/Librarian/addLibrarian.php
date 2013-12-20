2c8db5c51ac66d0231c49c318b39945395165ffe
Rhianne Hadfield <dawg_rh@hotmail.com>
2013-12-20 12:19:12 -0700

    Added Session

--- Processing/Librarian/addLibrarian.php
+++ Processing/Librarian/addLibrarian.php
@@ -1,5 +1,17 @@
 <?php
 
+session_start();
+        // If the user is not logged then the user will be set to
+        if (isset($_SESSION['loggedIn']) && isset($_SESSION['username'])) {
+          if($_SESSION["loggedIn"] !=1)
+          {
+              header("Location: MainPage.php");
+          }
+        }
+        else{
+            header("Location: MainPage.php");
+        }
+
 include '../../Headers/dbConnect.php';
 
 $name = mysql_real_escape_string($_POST['name']);
