4506a7c0e6f45516f42ee6aa1c8e0b369287017a
Rhianne Hadfield <dawg_rh@hotmail.com>
2013-12-20 16:56:14 -0700

    Fixed Session

--- catalogue.php
+++ catalogue.php
@@ -1,14 +1,4 @@
-<?php
-session_start();
-// If the user is not logged then the user will be set to the main page
-if (isset($_SESSION['loggedIn']) && isset($_SESSION['username'])) {
-    if ($_SESSION["loggedIn"] != 1) {
-        header("Location: MainPage.php");
-    }
-} else {
-    header("Location: MainPage.php");
-}
-?>
+
 
 <script type="text/javascript">
     $(function() {
