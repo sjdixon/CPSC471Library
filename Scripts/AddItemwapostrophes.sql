3494e461364f84456fdfa620d3d8ea5197fe1d29
Gabbeh <gabichan@me.com>
2013-12-20 13:22:30 -0700

    Adding script to test bug #10 in list

---
+++ Scripts/AddItemwapostrophes.sql
@@ -0,0 +1,9 @@
+Use library;
+insert into Item values (@v1,'Book','Children\'s Section','Treasure Island','1779',0,"Childrens", "Children");
+insert into Book (libraryCode, ISBN, authors) values (@v1, 123e4321, "Robert Louis Stevenson");
+
+
+
+
+
+
