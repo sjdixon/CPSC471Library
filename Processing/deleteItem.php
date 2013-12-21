e1d13a7b79843d4c50fc8e1fa20354bc188c9b16
Gabbeh <gabichan@me.com>
2013-12-21 14:22:24 -0700

    Delete Item form (loans and holds should be deleted?)

--- Processing/deleteItem.php
+++ Processing/deleteItem.php
@@ -13,7 +13,7 @@
 	
 	//deleteItem.php - written by Gaby Comeau
 	//PHP script to delete an item from the database
-	header("Location: ../App_Index.php",TRUE,303);
+	//header("Location: ../App_Index.php",TRUE,303);
         include '../Headers/dbConnect.php';
 	
 	$item = mysql_real_escape_string($_POST['lCode3']);
@@ -23,31 +23,49 @@
 	$type = $typerow[0];
 	//echo "$type<br>";
 	
-	//first, delete the entry from the type table
+        //delete the entry from the loans table
+	$query = "DELETE FROM Loan WHERE libraryCode = '$item'";
+	//echo "$query<br>";
+	$results = mysql_query($query);
+	if(!$results){
+            echo "could not delete from $item table<br />";
+            trigger_error(mysql_error(), E_USER_ERROR);
+        }
+        
+        //delete the entry from the holds table
+	$query = "DELETE FROM Hold WHERE libraryCode = '$item'";
+	//echo "$query<br>";
+	$results = mysql_query($query);
+	if(!$results){
+            echo "could not delete from $item table<br />";
+            trigger_error(mysql_error(), E_USER_ERROR);
+        }
+        
+	// delete the entry from the type table
 	$query = "DELETE FROM $type WHERE libraryCode = '$item'";
 	//echo "$query<br>";
 	$results = mysql_query($query);
 	if(!$results){
-      	echo "could not delete from $item table<br />";
-    	trigger_error(mysql_error(), E_USER_ERROR);
-    }
+            echo "could not delete from $item table<br />";
+            trigger_error(mysql_error(), E_USER_ERROR);
+        }
 	
-	//Second, remove all item instances
+	//remove all item instances
 	$query = "DELETE FROM Item_Instance WHERE libraryCode = '$item'";
 	echo "$query<br>";
 	$results1 = mysql_query($query);
 	if(!$results1){
-      	echo "could not delete from Instance table<br />";
-    	trigger_error(mysql_error(), E_USER_ERROR);
-    }
+            echo "could not delete from Instance table<br />";
+            trigger_error(mysql_error(), E_USER_ERROR);
+        }
 	
 	//Finally, delete the item table entry
 	$query = "DELETE FROM Item WHERE libraryCode = '$item'";
 	echo "$query<br>";
 	$results2 = mysql_query($query);
 	if(!$results2){
-      	echo "could not delete from Item table <br />";
-    	trigger_error(mysql_error(), E_USER_ERROR);
-    }
+            echo "could not delete from Item table <br />";
+            trigger_error(mysql_error(), E_USER_ERROR);
+        }
 
 ?>
