4971e5e24e393870310a30e6ce4bf5eb4b1246a0
Rhianne Hadfield <dawg_rh@hotmail.com>
2013-12-20 21:35:51 -0700

    Added missing include statment

--- Processing/getItem.php
+++ Processing/getItem.php
@@ -14,10 +14,7 @@
 	//getItem.php - written by Gaby Comeau
 	//PHP script to find the information of an item to modify
 	
-	$host = "localhost";
-	$user = "ubuntu";
-	$pass = "stephen123";				
-	mysql_connect($host, $user, $pass) or die("Could not connect: " . mysql_error());
+	include "./Headers/dbConnect.php";
 	mysql_select_db("library");	
 	$id = intval(mysql_real_escape_string($_GET['libID']));
 	$result = mysql_query("SELECT * FROM Item WHERE libraryCode='$id'");
