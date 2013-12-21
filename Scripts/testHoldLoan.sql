f51f7c2e62310f069865caf5938bef9995259b68
Stephen Dixon <sjdixon2@gmail.com>
2013-12-21 14:52:13 -0700

    Updated form.

--- Scripts/testHoldLoan.sql
+++ Scripts/testHoldLoan.sql
@@ -1,4 +1,4 @@
 use library;
-insert into Loan (pAccount, libraryCode, stocknum, dateLoaned, expiryDate) values (100, 8, 1, curdate() - 1 month, curdate() - interval 1 week);
-insert into Loan (pAccount, libraryCode, stocknum, dateLoaned, expiryDate) values (90, 8, 2, curdate() - 1 month, curdate() + interval 1 week);
-insert into Hold (pAccount, libraryCode, stocknum, dateHeld, availDate, shelfDate, expiryDate, returnedDate, pickupDate) values (100,8,2, curdate() - interval 1 year, curdate()- interval 50 week, curdate() - interval 48 week, curdate() - interval 45 week, curdate() - interval 44 week, NULL);
+insert into Loan (pAccount, libraryCode, stocknum, dateLoaned, dateDue) values (100, 4, 1, curdate() - interval 1 month, curdate() - interval 1 week);
+insert into Loan (pAccount, libraryCode, stocknum, dateLoaned, dateDue) values (90, 4, 2, curdate() - interval 1 month, curdate() + interval 1 week);
+insert into Hold (pAccount, libraryCode, stocknum, dateHeld, availDate, shelfDate, expiryDate, removalDate, pickupDate) values (100,4,2, curdate() - interval 1 year, curdate()- interval 50 week, curdate() - interval 48 week, curdate() - interval 45 week, curdate() - interval 44 week, NULL);
